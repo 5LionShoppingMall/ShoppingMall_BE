5a696ef84073186623c038630f23c2c607317b18
kyumho kim <rlaruagh030@gmail.com>
2023-11-15 23:20:35 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/com/ll/lion_shoppingmall_be/Application.java
@@ -0,0 +1,13 @@
+package com.ll.lion_shoppingmall_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
