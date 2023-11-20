d6d6bc384b1fdd625e5ba94280372cf66c8ffe8e
kyumho kim <rlaruagh030@gmail.com>
2023-11-20 21:30:46 +0900

    application-security.yml 파일 생성 및 dev 환경에서 사용할 수 있도록 설정

--- src/test/java/com/ll/lion/security/JwtTokenProviderTest.java
+++ src/test/java/com/ll/lion/security/JwtTokenProviderTest.java
@@ -1,4 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.ll.lion.security;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+@SpringBootTest
 class JwtTokenProviderTest {
-  
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Test
+    public void showSecret(){
+        System.out.println(jwtTokenProvider.getSecretKey());
+    }
 }
