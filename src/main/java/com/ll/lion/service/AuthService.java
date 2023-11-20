3b7d8ebca45f673e8412d8eae5d5af2f20bfd51f
kyumho kim <rlaruagh030@gmail.com>
2023-11-21 00:32:09 +0900

    feat : AuthService 추가

--- src/main/java/com/ll/lion/service/AuthService.java
+++ src/main/java/com/ll/lion/service/AuthService.java
@@ -1,2 +1,40 @@
-package com.ll.lion.service;public class AuthService {
+package com.ll.lion.service;
+
+import com.ll.lion.security.JwtTokenProvider;
+import com.ll.lion.security.UserDetailsServiceImpl;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
+
+    public String authenticate(String email, String password) {
+        UserDetails userDetails;
+        try {
+            userDetails = userDetailsService.loadUserByUsername(email);
+        } catch (UsernameNotFoundException e) {
+            return null; // 사용자가 존재하지 않는 경우
+        }
+
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {
+            // 로그인 성공 시 JWT 토큰 생성
+            return jwtTokenProvider.createToken(email, List.of("USER"));
+        } else {
+            return null; // 비밀번호가 일치하지 않는 경우
+        }
+    }
+
 }
