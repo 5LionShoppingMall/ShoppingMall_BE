090df5b4a0a0dc7f066917542d5c42c9e4c8938e
kyumho kim <rlaruagh030@gmail.com>
2023-11-20 21:29:40 +0900

    refacotr: SecurityConfig JWT 사용할 수 있게 재설정

--- src/main/java/com/ll/lion/security/InvalidJwtAuthenticationException.java
+++ src/main/java/com/ll/lion/security/InvalidJwtAuthenticationException.java
@@ -1,2 +1,8 @@
-package com.ll.lion.security;public class InvalidJwtAuthenticationException {
+package com.ll.lion.security;
+
+public class InvalidJwtAuthenticationException extends RuntimeException {
+
+    public InvalidJwtAuthenticationException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/ll/lion/security/SecurityConfig.java
+++ src/main/java/com/ll/lion/security/SecurityConfig.java
@@ -1,35 +1,49 @@
-package com.ll.lion.config;
+package com.ll.lion.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtTokenProvider;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtTokenProvider);
         http
-                .authorizeRequests(authorize -> authorize
+                .csrf(c -> c.disable())
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests(a -> a
                         .anyRequest().authenticated()
-                )
-                .formLogin(withDefaults())
-                .httpBasic(withDefaults());
+                );
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public UserDetailsService users() {
         UserDetails user = User.builder()
                 .username("user")
-                .password("{noop}password")
+                .password(passwordEncoder().encode("password"))
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
