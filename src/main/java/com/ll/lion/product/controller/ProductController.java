c87449f90c2dbc2a771d5fc18eda8cb10c024ad0
daseul-jang <devzzang94@gmail.com>
2023-11-21 17:49:37 +0900

    Controller 생성

---
+++ src/main/java/com/ll/lion/product/controller/ProductController.java
@@ -0,0 +1,11 @@
+package com.ll.lion.product.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+}
