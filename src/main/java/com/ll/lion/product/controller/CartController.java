58a5a9b9fd6346aa967b29ef76008556a046e3b6
geniushee <h1116jeon@gmail.com>
2023-11-23 17:07:46 +0900

    중간저장

--- src/main/java/com/ll/lion/product/controller/CartController.java
+++ src/main/java/com/ll/lion/product/controller/CartController.java
@@ -1,8 +1,10 @@
 package com.ll.lion.product.controller;
 
-import com.ll.lion.product.entity.Cart;
-import com.ll.lion.product.entity.Product;
+import com.ll.lion.product.dto.CartDto;
+import com.ll.lion.product.entity.CartItem;
+import com.ll.lion.product.service.CartItemService;
 import com.ll.lion.product.service.CartService;
+import com.ll.lion.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,16 @@ public class CartController {
     private final CartService cartService;
     private final CartItemService cartItemService;
 
+
     @GetMapping("")
-    public List<Product> showCart(){
-        Cart cart = cartService.getCart();
-        return
+    public void showCart(){
+        List<CartItem> testCart; //테스트용 카트
+        CartDto cartDto = new CartDto();
+        // 유저 정보를 받아와야함
+        cartDto.setCartOwner(new User());
+        cartDto.setCart(cartService.getCart(cartDto.getCartOwner()).get());
+        testCart = cartItemService.getItemList(cartDto);
+        System.out.println(testCart);
+
     }
 }
